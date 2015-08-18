0b0b5af477955ee800389ebdcb062b2c787347e5
AnkitaRani <ankita_rani@outlook.com>
2015-08-18 19:53:05 -0400

    09/27

--- OBPS/src/main/java/com/spring/myapp/views/controllers/CorporationViewController.java
+++ OBPS/src/main/java/com/spring/myapp/views/controllers/CorporationViewController.java
@@ -1,11 +1,14 @@
 package com.spring.myapp.views.controllers;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.spring.myapp.dao.CorporationDAO;
 import com.spring.myapp.dao.impl.CorporationDAOImpl;
 import com.spring.myapp.models.Corporation;
@@ -22,9 +25,12 @@ public class CorporationViewController {
 	 }
 	 
 	 @RequestMapping(value="listCorporation", method=RequestMethod.GET)
+	 @ResponseBody
 	 public String listCorporation() {
 		 List<Corporation> corporations = cvc.listCorporation();
 		 String temp = corporations.toString();
-		 return corporations.toString();
+		 HashMap<String, Object> hm = new HashMap<String, Object>();
+	        hm.put("corporations", corporations);
+	        return new Gson().toJson(hm);
 	 }
 }
--- OBPS/src/main/webapp/WEB-INF/view/listCorporation.jsp
+++ OBPS/src/main/webapp/WEB-INF/view/listCorporation.jsp
@@ -1,6 +1,12 @@
-<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ page language="java" contentType="text/html; charset=US-ASCII" pageEncoding="US-ASCII"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%-- <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+<%@ taglib prefix="eb" uri="http://www.empoweredbenefits.com/content.tld" %> --%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
 
 <html>
 <head>
